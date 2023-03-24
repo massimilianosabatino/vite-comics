b5118e7189e9596b77661436ffebfefdac18e903
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-03-24 01:30:20 +0100

    scaffolding subMenu

---
+++ src/components/SpecialLinkComponent.vue
@@ -0,0 +1,13 @@
+<script>
+    export default{};
+</script>
+<template>
+    <a :href="element.link">
+        <div>
+            <img :src="element.url" :alt="`Icon ${element.name}`">
+            <div class="text">{{ element.name }}</div>
+        </div>
+    </a>
+</template>
+<style lang="scss" scoped>
+</style>
--- src/components/SubMenuComponet.vue
+++ src/components/SubMenuComponet.vue
@@ -1,13 +1,67 @@
 <script>
-export default {}
+import SpecialLinkComponent from './SpecialLinkComponent.vue';
+export default {
+    components: {
+        SpecialLinkComponent
+    },
+    data() {
+        return {
+            cards: [
+                {
+                    name: 'digital comics',
+                    url: './src/assets/buy-comics-digital-comics.png',
+                    link: '#'
+                },
+                {
+                    name: 'dc merchandise',
+                    url: './src/assets/buy-comics-merchandise.png',
+                    link: '#'
+                },
+                {
+                    name: 'subscription',
+                    url: './src/assets/buy-comics-subscriptions.png',
+                    link: '#'
+                },
+                {
+                    name: 'comic shop locator',
+                    url: './src/assets/buy-comics-shop-locator.png',
+                    link: '#'
+                },
+                {
+                    name: 'dc power visa',
+                    url: './src/assets/buy-dc-power-visa.svg',
+                    link: '#'
+                },
+            ]
+        }
+    }
+}
 </script>
 <template>
-    <div></div>
+    <div class="sub-menu">
+        <div v-for="element in cards">
+            <a :href="element.link">
+                <div>
+                    <img :src="element.url" :alt="`Icon ${element.name}`">
+                    <div class="text">{{ element.name }}</div>
+                </div>
+            </a>
+        </div>
+    </div>
 </template>
 <style lang="scss" scoped>
     @use "./style/variables.scss" as *;
-    * {
+    .sub-menu {
         height: 155px;
         background-color: $primary-color;
+        display: flex;
+        justify-content: space-between;
+
+        div {
+            display: flex;
+            flex-wrap: nowrap;
+            align-items: center;
+        }
     }
+
 </style>
